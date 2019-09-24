840fb4eaf6b420e4aaee9b014ed908df1bbf16e6
Samuel Folledo <samuelfolledo@gmail.com>
2019-09-23 23:14:02 -0700

    [Page2] completed adding Ability, Armor, Hero class with fight method. Also added some text in README.md file

--- ReadMe.md
+++ ReadMe.md
@@ -1,1 +1,8 @@
+# Superhero-Team-Dueler
+Ever wondered which of your favorite super heroes would win in a fight? Team them up for battle in this introduction to object oriented programming.
+
+## By Samuel P. Folledo
+
+
+### Links
 https://www.makeschool.com/academy/track/standalone/superhero-team-dueler/superhero-objects
--- superheroes.py
+++ superheroes.py
@@ -1,31 +1,96 @@
 import random
 
+#Ability
 class Ability:
-    attack_strengthz
-        def __init__(self, name, attack_strength):
+    def __init__(self, name, strength):
         self.name = name
-        self.attack_strength = attack_strength
+        self.strength = strength
+
+    def attacks(self):
+        # damage = random.randint(0, self.attack_strength)
+        # print("damage is: ", damage)
+        damage = 0
+        damage += self.strength
+        return damage
+
+
+#Armor
+class Armor:
+    def __init__(self, name, max_block):
+        self.name = name
+        self.max_block = max_block 
+
+    def blocks(self):
+        block = random.randint(0, self.max_block)
+        print("block is: ", block)
+        return block
+    
+
+#Hero
+class Hero:
+    def __init__(self, name, current_health = 100):
+        self.name = name
+        self.current_health = current_health
+        self.abilities = list()
+        self.armors = list()
+        self.deaths = 0
+        self.kills = 0
+
+    def add_armor(self, armor):
+        self.armors.append(armor)
+
+    def add_ability(self, ability):
+        self.abilities.append(ability)
 
     def attack(self):
-        damage2 = random.randint(0,att)
-        print(damage2)
-        pass
+        total_attack = 0
+        for ability in self.abilities:
+            total_attack += ability.attacks()
+        # print("Total attack is: ",total_attack)
+        return total_attack
 
-class Armor:
-    def __init__(self, block):
-        self.block = block
-    
-class Hero:
-    def __init__(self, add_ability, attack, defend, take_damage, is_alive, fight):
-        self.add_ability = add_ability
-        self.attack = attack
-        self.defend = defend
-        self.take_damage = take_damage
-        self.is_alive = is_alive
-        self.fight = fight
+    def defend(self):
+        total_defense = 0
+        for armor in self.armors:
+            total_defense += armor.blocks()
+        # print("Total Defense is: ",total_defense)
+        return total_defense
+
+    def take_damage(self, damage):
+        damage_to_health = damage - self.defend()
+        # print("Damage to health is: ", damage_to_health)
+        self.current_health -= damage_to_health
+
+    def is_alive(self):
+        return self.current_health > 0 #returns True if health > 0
+            
+    def fight(self, opponent):
+        if len(self.abilities) == 0 and len(opponent.abilities) == 0:
+            print("Draw!")
+        else:
+            while self.is_alive() and opponent.is_alive():
+                opponent.take_damage(self.attack())
+                self.take_damage(opponent.attack())
+                if self.is_alive() and not opponent.is_alive(): #if self won and not opponent
+                    print(f"{self.name} won!")
+                elif not self.is_alive() and opponent.is_alive(): #if opponent won
+                    print(f"{opponent.name} won!")
+                else:
+                    print(f"{self.name} has {self.current_health}HP left\n{opponent.name} has {opponent.current_health}HP left")
 
     
-# if __name__ == "__main__":
-myDamage = Ability("Kobe", 100)
-print("\()")
-myDamage.attack()
+    
+if __name__ == "__main__":
+    hero1 = Hero("Superman")
+    hero2 = Hero("Batman")
+    ability1 = Ability("Laser eyes", 30)
+    ability2 = Ability("Freeze breath", 10)
+    ability3 = Ability("Bat mobile attack", 70)
+    ability4 = Ability("Batarang", 20)
+
+    hero1.add_ability(ability1)
+    hero1.add_ability(ability2)
+    hero2.add_ability(ability3)
+    hero2.add_ability(ability4)
+
+    hero1.fight(hero2)
