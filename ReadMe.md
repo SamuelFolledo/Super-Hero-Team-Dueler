47f427e1e7fad5bfe1ca828145d47f8102ac01d4
Samuel Folledo <samuelfolledo@gmail.com>
2019-09-26 17:22:10 -0700

    [finished] finished page 3. [add] hero_test.py, [completed] made sure all tests works

--- ReadMe.md
+++ ReadMe.md
@@ -2,13 +2,21 @@
 Ever wondered which of your favorite super heroes would win in a fight? Team them up for battle in this introduction to object oriented programming.
 
 ## Key Terms
+### Page 1 Object-Oriented Programming: Dogs
 - **Instantiation** - the process of creating an object in memory from the class definition
-- **Constructor** - allows us to add a property to the class upon initialization
+
+### Page 2 Build Ability and Hero Classes
+- **Constructor** - allows us to add a property to the class upon initialization. 
                 `__init__` is a special method called a constructor that runs whenever a new instance of a class is created
+
+### Page 3 Instance
 - **Inheritance** - allows us for additional ways to reuse code
 - **superclass** - the parent class
 - **subclass** - a child class that inherited a superclass and all of its parent's properties and methods
 - **Polymorphism** - allows us to use different implementations of the same method
+- __Method overriding__ - a form of polymorphism which and specify/redefine/override a different functionality for methods that are inherited from the superclass
+- __Test Driven Development (TDD)__ - is another way of imagining the end result before you dive into coding
+                                    - concept of writing code that verifies the behaviors we want our program to perform
 
 
 ## By [Samuel P. Folledo](https://github.com/SamuelFolledo)
--- superheroes.py
+++ superheroes.py
@@ -2,16 +2,17 @@ import random
 
 #Ability
 class Ability:
-    def __init__(self, name, strength):
+    def __init__(self, name, max_damage):
         self.name = name
-        self.strength = strength
+        self.max_damage = max_damage
 
-    def attacks(self):
-        # damage = random.randint(0, self.attack_strength)
-        # print("damage is: ", damage)
-        damage = 0
-        damage += self.strength
-        return damage
+    def attack(self):
+        return random.randint(0, self.max_damage)
+
+class Weapon(Ability):
+    def attack(self): #returns a random value from half to full power of the weapon
+        return random.randint(self.max_damage // 2, self.max_damage) #floor division (//) divides and returns the integer value of the quoties; dumping digits after the decimal
+
 
 
 #Armor
@@ -45,20 +46,17 @@ class Hero:
     def attack(self):
         total_attack = 0
         for ability in self.abilities:
-            total_attack += ability.attacks()
-        # print("Total attack is: ",total_attack)
+            total_attack += ability.attack()
         return total_attack
 
     def defend(self):
         total_defense = 0
         for armor in self.armors:
             total_defense += armor.blocks()
-        # print("Total Defense is: ",total_defense)
         return total_defense
 
     def take_damage(self, damage):
         damage_to_health = damage - self.defend()
-        # print("Damage to health is: ", damage_to_health)
         self.current_health -= damage_to_health
 
     def is_alive(self):
@@ -79,8 +77,33 @@ class Hero:
                     print(f"{self.name} has {self.current_health}HP left\n{opponent.name} has {opponent.current_health}HP left")
 
     
-    
-if __name__ == "__main__":
+class Team():
+    def __init__(self, name):
+        self.heroes = list()
+        self.name = name
+
+    def add_hero(self, hero):
+        self.heroes.append(hero)
+
+    def remove_hero(self, hero_name):
+        hero_names_list = list()
+        for hero in self.heroes:
+            hero_names_list.append(hero.name)
+        if hero_name in hero_names_list: #if we have hero, get index and pop
+            hero_index = hero_names_list.index(hero_name)
+            self.heroes.pop(hero_index)
+        else:
+            return 0
+
+    def view_all_heroes(self):
+        print("The heroes are:")
+        if len(self.heroes) > 0:
+            for hero in self.heroes:
+                print(f"\n- {hero.name}")
+        else:
+            print("We have no heroes to view")
+
+def test_fight():
     hero1 = Hero("Superman")
     hero2 = Hero("Batman")
     ability1 = Ability("Laser eyes", 30)
@@ -94,3 +117,21 @@ if __name__ == "__main__":
     hero2.add_ability(ability4)
 
     hero1.fight(hero2)
+
+
+
+if __name__ == "__main__":
+    hero1 = Hero("Superman")
+    hero2 = Hero("Batman")
+    hero3 = Hero("Spiderman")
+    hero4 = Hero("Ryu")
+    marvel_team = Team("Marvel")
+    marvel_team.add_hero(hero1) 
+    marvel_team.add_hero(hero2) 
+    marvel_team.add_hero(hero3)
+    marvel_team.view_all_heroes() 
+    marvel_team.remove_hero(hero4) 
+    marvel_team.remove_hero("Spiderman")  
+    # marvel_team.add_hero("kobe")
+
+    marvel_team.view_all_heroes()
